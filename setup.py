231a89eacc24a1772604dd684460508435268c7b
alyucu6 <allisonycui@gmail.com>
2024-12-09 21:21:15 -0500

    modified name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="shared-logging-middleware",
+    name="middleware",
     version="1.0.0",
     packages=find_packages(),
     install_requires=["fastapi", "uvicorn"],

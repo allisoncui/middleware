0c3187c7e6093a5aa93161b8b4bdb538f1265e0a
alyucu6 <allisonycui@gmail.com>
2024-12-09 22:01:06 -0500

    middleware structure changed

---
+++ middleware/__init__.py
@@ -0,0 +1,1 @@
+from .middleware import log_request_response
--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name="middleware",
     version="1.0.0",
     packages=find_packages(),
-    install_requires=["fastapi", "uvicorn"],
+    install_requires=[
+        "fastapi",
+        "uvicorn"
+    ],
+    description="A reusable middleware for logging requests and responses"
 )
